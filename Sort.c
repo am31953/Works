54abc9b831cbaf1f19f725446817d7ae92e88c2c
am31953 <147327332+am31953@users.noreply.github.com>
2024-01-05 19:48:52 +0100

    Sort.c

---
+++ Sort.c
@@ -0,0 +1,16 @@
+sort1 uses:
+Bubble Sort
+
+How do you know?:
+Takes less amount of time when running sorted texts compared to sort3
+
+sort2 uses:
+Merge Sort
+
+How do you know?: Is the fastest from others
+
+sort3 uses:
+Selection Sort
+
+How do you know?:
+Takes lots of time when running on sorted texts compared to sort1
