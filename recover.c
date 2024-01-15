3f1db34bdcec8815efaba811958090f2c84f8bb2
am31953 <147327332+am31953@users.noreply.github.com>
2024-01-15 22:35:56 +0100

    recover.c

--- recover.c
+++ recover.c
@@ -1,75 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <stdint.h>
-
-typedef uint8_t BYTE;
-
+#define BLOCK_SIZE 512
 
 int main(int argc, char *argv[])
 {
-    // Check if there are two arguments
-    if (argc != 2)
+    if (argc !=2)
     {
-        fprintf(stderr, "Usage: ./recover image\n");
+        printf("Usage: ./recover IMAGE\n");
         return 1;
     }
 
-    // Open the memory card
-    FILE *file = fopen(argv[1], "r");
-
-    // Check if there is something in the file
-    if (file == NULL)
+    char *file = argv[1];
+    FILE *raw_file = fopen(file, "r");
+    if(raw_file == NULL)
     {
-        fprintf(stderr, "Invalid file\n");
+        printf ("Could not open %s.\n", file);
         return 1;
     }
 
-    int jpg_counter = 0;
-    // Array of each memory card block
-    BYTE buffer[512];
-    // File name is stored in 1 byte
-    char filename[8];
-    FILE *img;
+    bool found_jpg = false;
+    int jpg_count = 0;
+    uint8_t buffer[BLOCK_SIZE];
+    char jpg_name[8];
+    FILE *outptr = NULL;
 
-    // While it is possible to read inside the file
-    while (fread(&buffer, sizeof(buffer), 1, file))
+    while (fread(buffer, BLOCK_SIZE, 1, raw_file)==1)
     {
-        // If the file is jpeg
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            // If it's the first image
-            if (jpg_counter == 0)
+            if (found_jpg)
             {
-                // Creates the formatted image name, indicated by its number
-                sprintf(filename, "%03i.jpg", jpg_counter);
-                // Open the image to write on it
-                img = fopen(filename, "w");
-                // Write the image
-                fwrite(&buffer, sizeof(buffer), 1, img);
-
-                jpg_counter++;
+                fclose(outptr);
+            }
+            else
+            {
+                found_jpg = true;
             }
 
-            // If you already have another image
-            else if (jpg_counter > 0)
+            sprintf(jpg_name, "%03d.jpg", jpg_count);
+            outptr = fopen(jpg_name, "w");
+            if (outptr == NULL)
             {
-                // Close the previous image
-                fclose(img);
-
-                sprintf(filename, "%03i.jpg", jpg_counter);
-                img = fopen(filename, "w");
-                fwrite(&buffer, sizeof(buffer), 1, img);
-
-                jpg_counter++;
+                fclose(raw_file);
+                printf("Could not create %s. \n", jpg_name);
+                return 3;
             }
+            jpg_count++;
         }
-        else if (jpg_counter > 0)
+
+        if (found_jpg)
         {
-            // Keep writing, if an image is already being written
-            fwrite(&buffer, sizeof(buffer), 1, img);
+            fwrite(buffer, BLOCK_SIZE, 1, outptr);
         }
     }
 
-    fclose(file);
-    fclose(img);
+    fclose(raw_file);
+    if(found_jpg)
+    {
+        fclose(outptr);
+    }
+
+    return 0;
+
 }
